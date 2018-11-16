0b355486ca9ae3f640e56025b1f69bb4fcd09626
Kramerbaf <nds.kram@gmail.com>
2018-11-16 16:18:13 +0100

    updated wwObject.register

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ const wwEnableMetrics = true;
 const addComponent = function () {
     if (window.vm) {
 
-        wwLib.wwObject.registerContentType(
-            name,
-            {
+        wwLib.wwObject.register({
+            content: {
                 type: name,
                 data: {
                     text: {
@@ -23,8 +22,13 @@ const addComponent = function () {
                     fullWidth: false
                 }
             },
-            wwEnableMetrics
-        );
+            enableMetrics: wwEnableMetrics,
+            /* wwManager:start */
+            cmsOptions: {
+            }
+            /* wwManager:end */
+        });
+
 
         window.vm.addComponent(name, wwObject);
 

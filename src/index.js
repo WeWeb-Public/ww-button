d1b25164d37d763ad8cff51f0e329bfbcc752e01
Kramerbaf <nds.kram@gmail.com>
2018-11-28 12:10:15 +0100

    updated wwobjects options

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import wwObject from './wwObjectButton.vue'
 
 const name = "ww-button";
-const wwEnableMetrics = true;
 
 const addComponent = function () {
     if (window.vm) {
@@ -22,7 +21,11 @@ const addComponent = function () {
                     fullWidth: false
                 }
             },
-            enableMetrics: wwEnableMetrics,
+            upsales: {
+                wwAnalytics: {
+                    click: true
+                }
+            },
             /* wwManager:start */
             cmsOptions: {
             }
